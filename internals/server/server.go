b78023237957c6f4db2416cb438c2de65131825a
Nikunj Saini <nikunj902782@gmail.com>
2026-06-07 16:12:30 +0530

    feat: add ping commands contructor

--- internals/server/server.go
+++ internals/server/server.go
@@ -1,65 +1,149 @@
 package server
 
 import (
+	"bufio"
+	"errors"
 	"fmt"
+	"io"
+	"log"
 	"net"
+	"strings"
+
+	"redis-demo/internals/commands"
+	"redis-demo/internals/protocols"
 )
 
 type Server struct {
-	addr string
+	addr     string
+	listener net.Listener
+	registry map[string]commands.Command
 }
 
-func New(addr string) *Server {
-	return &Server{
-		addr: addr,
+func NewServer(addr string) *Server {
+	s := &Server{
+		addr:     addr,
+		registry: make(map[string]commands.Command),
 	}
+	
+	s.registry["PING"] = &commands.PingCommand{}
+
+	return s
 }
 
 func (s *Server) Start() error {
-	listener, err := net.Listen("tcp", s.addr)
+	var err error
+	s.listener, err = net.Listen("tcp", s.addr)
 	if err != nil {
 		return err
 	}
+	defer s.listener.Close()
 
-	defer listener.Close()
-
-	fmt.Printf("Server listening on ", s.addr)
+	fmt.Printf("Redis clone server listening on %s\n", s.addr)
 
 	for {
-		conn, err := listener.Accept()
+		conn, err := s.listener.Accept()
 		if err != nil {
-			fmt.Printf("accept error: ", err)
+			log.Println("Connection accept error:", err)
 			continue
 		}
-
-		fmt.Printf("new client: ", conn.RemoteAddr())
-
 		go s.handleConnection(conn)
 	}
 }
 
 func (s *Server) handleConnection(conn net.Conn) {
 	defer func() {
-		fmt.Printf("client disconnected: ", conn.RemoteAddr())
+		fmt.Printf("client disconnected: %v\n", conn.RemoteAddr())
 		conn.Close()
 	}()
 
-	buf := make([]byte, 4096)
+	reader := bufio.NewReader(conn)
 
 	for {
-		n, err := conn.Read(buf)
+		cmd, err := s.readCommand(reader)
 		if err != nil {
+			if err == io.EOF {
+				return 
+			}
+			s.respondError(err, conn)
 			return
 		}
 
-		data := buf[:n]
-
-
-		fmt.Printf("received: ", data)
-
-		_, err = conn.Write([]byte("+OK\r\n"))
-		if err != nil {
-			return
-		}
+		s.respond(cmd, conn)
 	}
+}
+
+func (s *Server) readCommand(reader *bufio.Reader) (*commands.RedisCmd, error) {
+	
+	line, err := reader.ReadBytes('\n')
+	if err != nil {
+		return nil, err
+	}
+
+	if len(line) == 0 || line[0] != '*' {
+		return nil, errors.New("ERR protocol error: expected array start '*'")
+	}
+
+	
+	fullData := append([]byte{}, line...)
+	
+
+	count := 0
+	for i := 1; i < len(line) && line[i] != '\r'; i++ {
+		count = count*10 + int(line[i]-'0')
+	}
+
+	
+	for i := 0; i < count; i++ {
+		
+		lenLine, err := reader.ReadBytes('\n')
+		if err != nil {
+			return nil, err
+		}
+		fullData = append(fullData, lenLine...)
+
+		
+		strLen := 0
+		for j := 1; j < len(lenLine) && lenLine[j] != '\r'; j++ {
+			strLen = strLen*10 + int(lenLine[j]-'0')
+		}
+
+		payload := make([]byte, strLen+2)
+		_, err = io.ReadFull(reader, payload)
+		if err != nil {
+			return nil, err
+		}
+		fullData = append(fullData, payload...)
+	}
+
+	
+	tokens, err := protocols.DecodeArrayString(fullData)
+	if err != nil {
+		return nil, err
+	}
+
+	if len(tokens) == 0 {
+		return nil, errors.New("empty command")
+	}
+
+	return &commands.RedisCmd{
+		Cmd:  strings.ToUpper(tokens[0]),
+		Args: tokens[1:],
+	}, nil
+}
+
+func (s *Server) respond(cmd *commands.RedisCmd, conn net.Conn) {
+	
+	executor, ok := s.registry[cmd.Cmd]
+	if !ok {
+		s.respondError(errors.New("ERR unknown command"), conn)
+		return
+	}
+
+	if err := executor.Execute(conn, cmd.Args); err != nil {
+		s.respondError(err, conn)
+	}
+}
+
+func (s *Server) respondError(err error, conn net.Conn) {
+	_, _ = conn.Write([]byte(fmt.Sprintf("-%s\r\n", err.Error())))
 }
