d0ae6ec0edf477e26f81c7416bfc68f027bfd9e5
Nikunj Saini <nikunj902782@gmail.com>
2026-06-10 11:58:37 +0530

    changes import

--- main.go
+++ main.go
@@ -2,14 +2,17 @@ package main
 
 import (
 	"log"
+	"time"
 
-	"redis-demo/internals/server"
 	"redis-demo/internals/db"
+	"redis-demo/server"
 )
 
 func main() {
 	database := db.NewDB()
-	srv := server.NewServer(":6379" , database)
+	database.StartCleanup(1 * time.Second)
+
+	srv := server.NewServer(":6379", database)
 	if err := srv.Start(); err != nil {
 		log.Fatal(err)
 	}
